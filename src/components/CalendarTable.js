383af8943f29831639304e3d26d93ceba8cb12c3
Rohit Saxena <mastermind1234321@gmail.com>
2019-02-10 16:12:06 -0600

    added Month Year functinoality

--- src/components/CalendarTable.js
+++ src/components/CalendarTable.js
@@ -1,24 +1,80 @@
-import React from "react";
+import React, { Component } from "react";
 import Table from "react-bootstrap/Table";
+import { MONTHS } from './Constants';
+import Button from "react-bootstrap/Button";
+import Calendar from 'react-calendar';
 
-const CalendarTable = props => (
-  <div>
-    <div>
-      <h3>{props.title}</h3>
-      <Table bordered variant="dark">
-        <thead>
-          <tr>
-            {props.columnNames.map((dayObject, i) => (
-              <th key={i} width={dayObject.width}>
-                {dayObject.day}
-              </th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>{props.children}</tbody>
-      </Table>
-    </div>
-  </div>
-);
+
+class CalendarTable extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedDay: 1,
+      selectedMonth: 0,
+      selectedYear: 2019,
+      selectedDate: new Date('January 2, 2019')
+    }
+    // this.changeMonth = this.changeMonth.bind(this);
+    this.changeDate = this.changeDate.bind(this);
+  }
+
+  changeDate = code => {
+    const selectedDate = this.state.selectedDate;
+
+    switch(code) {
+      case "<":
+        selectedDate.setMonth(this.state.selectedDate.getMonth() - 1);
+        break;
+      case ">":
+        selectedDate.setMonth(this.state.selectedDate.getMonth() + 1);
+        break;
+      case "<<":
+        selectedDate.setFullYear(this.state.selectedDate.getFullYear() - 1);
+        break;
+      case ">>":
+        selectedDate.setFullYear(this.state.selectedDate.getFullYear() + 1);
+        break;
+      default:
+        break;
+    }
+    this.setState({ selectedDate });
+  }
+  
+  render() {
+    const selectedMonth = MONTHS[this.state.selectedDate.getMonth()].name;
+    return (
+      <div>
+        <Button variant="link" onClick={() => this.changeDate("<<")}>
+          {"<<"}
+        </Button>
+        <Button variant="link" onClick={() => this.changeDate("<")}>
+          {"<"}
+        </Button>
+        {selectedMonth} {this.state.selectedDate.getFullYear()}
+        <Button variant="link" onClick={() => this.changeDate(">")}>
+          {">"}
+        </Button>
+        <Button variant="link" onClick={() => this.changeDate(">>")}>
+          {">>"}
+        </Button>
+        <div>
+          <h3>{this.props.title}</h3>
+          <Table bordered variant="dark">
+            <thead>
+              <tr>
+                {this.props.columnNames.map((dayObject, i) => (
+                  <th key={i} width={dayObject.width}>
+                    {dayObject.day}
+                  </th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>{this.props.children}</tbody>
+          </Table>
+        </div>
+      </div>
+    );
+  }
+};
 
 export default CalendarTable;
--- src/components/Month.js
+++ src/components/Month.js
@@ -13,40 +13,34 @@ class Month extends Component {
       currentMonthValue: 0,
       currentMonthObject: MONTHS[0]
     };
-    this.changeMonth = this.changeMonth.bind(this);
+    // this.changeMonth = this.changeMonth.bind(this);
   }
 
-  changeMonth = direction => {
-    /*adds or subtracts 1 from currentMonth. Loops if value is over 12 or under 1*/
-    const current = this.state.currentMonthValue;
-    var newMonth;
-    switch (direction) {
-      case "<":
-        newMonth = (current - 1 >= 0 && current - 1) || 11;
-        break;
-      case ">":
-        newMonth = (current + 1 < 12 && current + 1) || 0;
-        break;
-      default:
-        newMonth = current;
-        break;
-    }
-    this.setState({
-      currentMonthValue: newMonth,
-      currentMonthObject: MONTHS[newMonth]
-    });
-  };
+  // changeMonth = direction => {
+  //   /*adds or subtracts 1 from currentMonth. Loops if value is over 12 or under 1*/
+  //   const current = this.state.currentMonthValue;
+  //   var newMonth;
+  //   switch (direction) {
+  //     case "<":
+  //       newMonth = (current - 1 >= 0 && current - 1) || 11;
+  //       break;
+  //     case ">":
+  //       newMonth = (current + 1 < 12 && current + 1) || 0;
+  //       break;
+  //     default:
+  //       newMonth = current;
+  //       break;
+  //   }
+  //   this.setState({
+  //     currentMonthValue: newMonth,
+  //     currentMonthObject: MONTHS[newMonth]
+  //   });
+  // };
 
   render() {
     return (
       <div>
-        <Button variant="link" onClick={() => this.changeMonth("<")}>
-          {"<"}
-        </Button>
-        {this.state.currentMonthObject.name}
-        <Button variant="link" onClick={() => this.changeMonth(">")}>
-          {">"}
-        </Button>
+        
         <CalendarTable title="Month View" columnNames={this.state.columnNames}>
           {[...Array(4)].map((_, i) => (
             <tr key={i}>
